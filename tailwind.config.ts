0c43c18c85dff6083f084c62bf486b907898c1a3
Kim <kimshengyong@gmail.com>
2024-12-23 00:07:45 +0800

    feat: add nav and stylings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,33 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        dimLight: '#6f6f6f',
+        dimDark: '#a0a0a0'
       },
+      typography: (theme: any) => ({
+        DEFAULT: {
+          css: {
+            maxWidth: '100%',
+            color: "var(--text-colour)",
+            'h1, h2, h3, h4, h5, h6': {
+              color: "var(--text-colour)",
+              marginBottom: '28px',
+              marginTop: '56px',
+            },
+            a: {
+              color: "var(--text-colour)",
+              transition: '100ms',
+              textDecorationColor: '#505050',
+              textDecorationThickness: '1px',
+              textUnderlineOffset: '2.5px',
+              '&:hover': {
+                textDecorationColor: '#6f6f6f',
+              },
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 } satisfies Config;
